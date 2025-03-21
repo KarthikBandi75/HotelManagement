e9eb219b176e58b5c76211ca287778057935cf25
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-03-21 16:36:43 +0530

    Update api.js

--- Frontend/src/Utils/api.js
+++ Frontend/src/Utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5776/api' });
+const API = axios.create({ baseURL: "https://hotelmanagement-backend-msbf.onrender.com/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
